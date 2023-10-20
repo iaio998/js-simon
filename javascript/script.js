92721176b6cbfa5a49c142f4aa0fd151a836c4df
iaio998 <depalatisiacopo@gmail.com>
2023-10-20 15:15:35 +0200

    Added form for user

--- javascript/script.js
+++ javascript/script.js
@@ -4,7 +4,7 @@ mySimonSays();
 function mySimonSays() {
   const start = document.getElementById("starting-btn");
   const numbers = document.getElementById("numbers");
-  const numToRemember = [];
+  const inputUser = document.getElementById("form");
 
   start.addEventListener("click", function () {
     myGenerateRandomNumber();
@@ -12,12 +12,14 @@ function mySimonSays() {
   });
 
   function myGenerateRandomNumber() {
+    const numToRemember = [];
     while (numToRemember.length < 5) {
       numToRemember.push(getRndInteger(1, 100));
     }
     numbers.classList.remove("d-none");
-    numbers.classList.add("fw-bold", "fs-3");
+    numbers.classList.add("fw-bold", "fs-3", "text-danger");
     numbers.innerHTML = numToRemember.join(", ");
+    return numToRemember;
   }
 
   function myDisappear() {
