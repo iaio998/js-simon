d654f340408356aebb470fdab3d528e22bce4b16
iaio998 <depalatisiacopo@gmail.com>
2023-10-20 14:58:55 +0200

    Added function disappear

--- javascript/script.js
+++ javascript/script.js
@@ -4,9 +4,23 @@ mySimonSays();
 function mySimonSays() {
   const start = document.getElementById("starting-btn");
   const numbers = document.getElementById("numbers");
+  const numToRemember = [];
 
   start.addEventListener("click", function () {
-    numbers.classList.remove("d-none");
-    numbers.innerHTML = getRndInteger(1, 100);
+    myGenerateRandomNumber();
+    const clock = setTimeout(myDisappear, 3000);
   });
+
+  function myGenerateRandomNumber() {
+    while (numToRemember.length < 5) {
+      numToRemember.push(getRndInteger(1, 100));
+    }
+    numbers.classList.remove("d-none");
+    numbers.classList.add("fw-bold", "fs-3");
+    numbers.innerHTML = numToRemember.join(", ");
+  }
+
+  function myDisappear() {
+    numbers.classList.add("d-none");
+  }
 }
