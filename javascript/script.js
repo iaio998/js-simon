1d7676e767b12cae92ceeb2eea5f1d2bbbcbc888
iaio998 <depalatisiacopo@gmail.com>
2023-10-20 17:47:40 +0200

    Added win or lose on console.log

--- javascript/script.js
+++ javascript/script.js
@@ -7,6 +7,7 @@ function mySimonSays() {
   const formUser = document.getElementById("form");
   const confirm = document.getElementById("confirm");
   const valueUser = document.getElementsByTagName("input");
+  const result = document.querySelector(".alert");
 
   const userNumbers = [];
   const checkArray = [];
@@ -14,21 +15,37 @@ function mySimonSays() {
   start.addEventListener("click", function () {
     //! Genera numeri random
     let numbersToWin = myGenerateRandomNumber();
-
+    console.log(numbersToWin);
     //! Fai sparire i numeri e fai apparire l'input per user
-    const clock = setTimeout(myDisappear, 1000);
+    const clock = setTimeout(myDisappear, 3000);
 
     //! Pusha i valori dati da user in array
     confirm.addEventListener("click", function () {
+      let counter = 0;
       for (let i = 0; i < valueUser.length; i++) {
+        let a;
         if (!isNaN(parseInt(valueUser[i].value))) {
           userNumbers.push(parseInt(valueUser[i].value));
         }
         if (numbersToWin[i] === userNumbers[i]) {
           checkArray.push(userNumbers[i]);
+          counter++;
         }
       }
       console.log(userNumbers);
+      formUser.classList.add("d-none");
+
+      if (checkArray.length === 5) {
+        console.log("Hai vinto");
+        result.classList.remove("d-none");
+        result.classList.add("alert-success");
+        result.innerHTML = "Hai vinto";
+      } else {
+        console.log("Hai perso");
+        result.classList.remove("d-none");
+        result.classList.add("alert-danger");
+        result.innerHTML = "Hai perso";
+      }
     });
   });
 
@@ -43,7 +60,8 @@ function mySimonSays() {
     }
     numbers.classList.remove("d-none");
     numbers.classList.add("fw-bold", "fs-3", "text-danger");
-    numbers.innerHTML = numToRemember.join(", ");
+    numbers.innerHTML = numToRemember.join(" - ");
+
     return numToRemember;
   }
 
@@ -54,6 +72,4 @@ function mySimonSays() {
     numbers.classList.add("d-none");
     formUser.classList.remove("d-none");
   }
-
-  function pushElements
 }
